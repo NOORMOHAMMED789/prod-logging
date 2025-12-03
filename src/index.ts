10b91e9566ee4be5693efd839b26d5d747c5445f
Noor <noor.mohammed@aprecomm.ai>
2025-12-03 15:29:32 +0530

    Error and initial endpoint for error simulation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
 import fastify from "fastify";
 
-const server = fastify();
+const server = fastify({
+    logger: {
+        level: 'info',
+        transport: {
+            target: 'pino-loki',
+            options: {
+                batching: true,
+                interval: 5,
+                host: 'http://localhost:3100',
+                labels: { app: 'prod-logging'}
+            }
+        }
+    }
+});
 const PORT = 3000;
 
 server.get('/', async(request, reply) => {
-    console.log("Received a request at /")
+    request.log.info('Root endpoint was called')
     return { message: 'Hello, fastify!'}
 })
 
+
+server.get('/error', async(request, reply) => {
+    request.log.info('Error endpoint was called')
+    throw new Error('This is a simulated error')
+})
 
 
 const start = async () => {
